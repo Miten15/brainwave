ecaa34144643b0fa1c85fd8216a755f1269306d0
Miten <miten2006@gmail.com>
2024-06-13 20:18:53 +0530

    Resuable Heading

--- src/components/Benefit.jsx
+++ src/components/Benefit.jsx
@@ -4,7 +4,10 @@ const Benefit = () => {
   return (
     <Section id="features">
       <div className="container relative z-2">
-        <Heading />
+        <Heading
+          className="md:max-w-md lg:max-w-2xl"
+          title="Chat Smarter, Not Harder with Brainwave"
+        />
       </div>
     </Section>
   );
