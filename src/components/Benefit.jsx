abbaa99d53e474bd23bf38864dd2cd803a43c581
Miten <miten2006@gmail.com>
2024-06-13 20:12:54 +0530

    Benefits section

---
+++ src/components/Benefit.jsx
@@ -0,0 +1,13 @@
+import Section from "./Section";
+import Heading from "./Heading";
+const Benefit = () => {
+  return (
+    <Section id="features">
+      <div className="container relative z-2">
+        <Heading />
+      </div>
+    </Section>
+  );
+};
+
+export default Benefit;
